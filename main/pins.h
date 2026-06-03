14651a539cb0aa75faaf637722d56eafeafa9127
Loress33 <lorenzoa@al.insper.edu.br>
2026-06-03 15:59:35 -0300

    Finalmente essa porra de IA ta funcionando

--- main/pins.h
+++
@@ -1,8 +0,0 @@
-#ifndef PINS_H
-#define PINS_H
-
-const int BTN_PIN_B = 15;
-const int BTN_PIN_O = 16;
-const int BTN_PIN_GRAB = 17;
-
-#endif // PINS_H
---
+++ src/pins.h
@@ -0,0 +1,8 @@
+#ifndef PINS_H
+#define PINS_H
+
+const int BTN_PIN_B = 15;
+const int BTN_PIN_O = 16;
+const int BTN_PIN_RESET = 17;
+
+#endif // PINS_H

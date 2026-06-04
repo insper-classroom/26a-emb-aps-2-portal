294471caa993483a817a0490b30c7e0c01690cf8
Bernardo Sarcinelli <bernardohns@al.edu.br>
2026-06-04 17:22:27 -0300

    LED RGB task

--- src/pins.h
+++ src/pins.h
@@ -6,5 +6,8 @@ const int BTN_PIN_O = 16;
 const int BTN_PIN_RESET = 17;
 const int BTN_PIN_CROUCH = 18;
 const int BTN_PIN_JUMP = 13;
-
+const int LED_PIN_R = 10;
+const int LED_PIN_G = 11;
+const int LED_PIN_B = 12;
+const int Feadback_Pin = 14
 #endif // PINS_H

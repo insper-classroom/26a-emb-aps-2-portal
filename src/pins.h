9fce24be691322374dc9b41fe002d7d9e4cd5053
Loress33 <lorenzoa@al.insper.edu.br>
2026-06-09 17:43:25 -0300

    Acho que arrumei o cppcheck

--- src/pins.h
+++ src/pins.h
@@ -1,14 +1,14 @@
 #ifndef PINS_H
 #define PINS_H
 
-const int BTN_PIN_B = 15;
-const int BTN_PIN_O = 16;
-const int BTN_PIN_RESET = 17;
-const int BTN_PIN_CROUCH = 18;
-const int BTN_PIN_JUMP = 13;
-const int LED_PIN_R = 10;
-const int LED_PIN_G = 11;
-const int LED_PIN_B = 12;
-const int Feadback_Pin = 14;
+#define BTN_PIN_B = 15;
+#define BTN_PIN_O = 16;
+#define BTN_PIN_RESET = 17;
+#define BTN_PIN_CROUCH = 18;
+#define BTN_PIN_JUMP = 13;
+#define LED_PIN_R = 10;
+#define LED_PIN_G = 11;
+#define LED_PIN_B = 12;
+#define Feadback_Pin = 14;
 
 #endif // PINS_H
